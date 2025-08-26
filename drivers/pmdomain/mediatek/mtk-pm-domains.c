735db3c52b9257e2d65b4aa35cae9c63b36d8a09
Arseniy Velikanov <me@adomerle.pw>
2025-08-26 05:41:09 +0400

    pmdomain: mediatek: Add MT6765 power domains
    Add support for SCPSYS power domains of MT6765.
    
    Signed-off-by: Arseniy Velikanov <me@adomerle.pw>

--- drivers/pmdomain/mediatek/mtk-pm-domains.c
+++ drivers/pmdomain/mediatek/mtk-pm-domains.c
@@ -17,6 +17,7 @@
 #include <linux/soc/mediatek/infracfg.h>
 
 #include "mt6735-pm-domains.h"
+#include "mt6765-pm-domains.h"
 #include "mt6795-pm-domains.h"
 #include "mt6893-pm-domains.h"
 #include "mt8167-pm-domains.h"
@@ -882,6 +883,10 @@ static const struct of_device_id scpsys_of_match[] = {
 	{
 		.compatible = "mediatek,mt6735-power-controller",
 		.data = &mt6735_scpsys_data,
+	},
+	{
+		.compatible = "mediatek,mt6765-power-controller",
+		.data = &mt6765_scpsys_data,
 	},
 	{
 		.compatible = "mediatek,mt6795-power-controller",
